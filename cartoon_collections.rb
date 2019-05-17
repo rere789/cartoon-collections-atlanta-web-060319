4272a20a20afa0c5eb23893b9782e0ed6b22df8d
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 19:58:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-     array.find do (food)
+     array.find do |food|
       if cheese_types.include?(food)
         return "#{food}"
     else
