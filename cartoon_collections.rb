ed3df55ec5eb4b18f45691d51b2a905fa09a9ec9
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 19:40:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.wiht_index.any? do |calls, index|
+  array.with_index.any? do |calls, index|
     calls[index] == 4
   end 
 end
