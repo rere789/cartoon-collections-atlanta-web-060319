11180ab1d2893dd76113f2f32d16e18d4d783f98
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 19:24:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  new_array = array.up
   array.collect do |name|
-  
+    return "#{name.capitalize}!"
   end 
-    
 end
 
 def long_planeteer_calls# code an argument here
